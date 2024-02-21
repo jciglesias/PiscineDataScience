85ff7d7e77e9cd8752b4014650e531c5e084d9e7
Juan Carlos Iglesias <jiglesia@student.42.fr>
2024-02-21 15:38:22 +0100

    diferent types of variables

--- Day00/ex02/table.py
+++ Day00/ex02/table.py
@@ -9,7 +9,7 @@ if __name__=="__main__":
     with open(f"/sgoinfre/goinfre/Perso/jiglesia/customer/data_2022_oct.csv", mode="r") as csvfile:
         csvreader = csv.reader(csvfile)
         header = next(csvreader)
-        cursor.execute(f'''CREATE TABLE IF NOT EXISTS {tableName.upper()}({header[0]} timestamp NOT NULL, {header[1]} varchar(50), {header[2]} int, {header[3]} float, {header[4]} int, {header[5]} varchar(100));''')
+        cursor.execute(f'''CREATE TABLE IF NOT EXISTS {tableName.upper()}({header[0]} timestamp NOT NULL, {header[1]} text, {header[2]} int, {header[3]} float, {header[4]} integer, {header[5]} varchar);''')
         cursor.copy_from(csvfile, table=f"{tableName.lower()}", sep=',', columns=header)
     conn.commit()
     conn.close()
