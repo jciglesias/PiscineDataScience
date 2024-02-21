bf6a4dc27019977ea2eba1c5a01b001b240662c7
Juan Carlos Iglesias <jiglesia@student.42.fr>
2024-02-21 16:43:44 +0100

    combine tables in one

---
+++ Day01/ex01/customers_table.py
@@ -0,0 +1,23 @@
+import psycopg2
+
+if __name__=="__main__":
+    conn = psycopg2.connect(database="piscineds", user='jiglesia', password='mysecretpassword', host='127.0.0.1')
+    conn.autocommit = True
+    cursor = conn.cursor()
+
+    cursor.execute('''
+                    CREATE TABLE customers AS
+                    SELECT *
+                    FROM data_2022_dec
+                    UNION ALL
+                    SELECT *
+                    FROM data_2022_nov
+                    UNION ALL
+                    SELECT *
+                    FROM data_2022_oct
+                    UNION ALL
+                    SELECT *
+                    FROM data_2023_jan;
+                    ''')
+    cursor.close()
+    conn.close()
