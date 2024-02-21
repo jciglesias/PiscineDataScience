41ae04e1077819589cc71b50a4d093a35671ff5c
Juan Carlos Iglesias <jiglesia@student.42.fr>
2024-02-21 14:23:55 +0100

    one table and multiple tables

--- Day00/ex03/automatic_table.py
+++ Day00/ex03/automatic_table.py
@@ -1,15 +1,28 @@
-import csv, psycopg2
+import os, csv, psycopg2, threading
+from tqdm import tqdm
 
-if __name__=="__main__":
+filepath = "/sgoinfre/goinfre/Perso/jiglesia/customer"
+
+def tables(file: str):
     conn = psycopg2.connect(database="piscineds", user='jiglesia', password='mysecretpassword', host='127.0.0.1')
     conn.autocommit = False
     cursor = conn.cursor()
 
-    tableName = "data_2022_oct"
-    with open(f"/sgoinfre/goinfre/Perso/jiglesia/customer/data_2022_oct.csv", mode="r") as csvfile:
+    tableName = file.replace(".csv", "")
+    with open(f"{filepath}/{file}", mode="r") as csvfile:
         csvreader = csv.reader(csvfile)
         header = next(csvreader)
         cursor.execute(f'''CREATE TABLE IF NOT EXISTS {tableName.upper()}({header[0]} timestamp NOT NULL, {header[1]} varchar(50), {header[2]} int, {header[3]} float, {header[4]} int, {header[5]} varchar(100));''')
         cursor.copy_from(csvfile, table=f"{tableName.lower()}", sep=',', columns=header)
     conn.commit()
     conn.close()
+
+if __name__=="__main__":
+    files = os.listdir(filepath)
+    threads = []
+    for file in files:
+        threads.append(threading.Thread(target=tables, args=(file,)))
+    for t in threads:
+        t.start()
+    for th in tqdm(threads, desc="Importing CSV"):
+        th.join()
