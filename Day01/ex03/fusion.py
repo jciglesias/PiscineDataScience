0c6bd1982776a1a5d56bb0dabb16f686f20ae4a0
Juan Carlos Iglesias <jiglesia@student.42.fr>
2024-02-23 15:01:53 +0100

    fill the item info with the most complete line

--- Day01/ex03/fusion.py
+++ Day01/ex03/fusion.py
@@ -2,7 +2,7 @@ import psycopg2
 
 if __name__ == "__main__":
     conn = psycopg2.connect(database="piscineds", user='jiglesia', password='mysecretpassword', host='127.0.0.1')
-    conn.autocommit = True
+    conn.autocommit = False
     cursor = conn.cursor()
 
     # cursor.execute('''
@@ -16,14 +16,24 @@ if __name__ == "__main__":
                     ADD COLUMN category_code VARCHAR,
                     ADD COLUMN brand VARCHAR;
                    ''')
+    
     cursor.execute('''
                     UPDATE customers AS c
                     SET
-                        category_id = i.category_id,
-                        category_code = i.category_code,
-                        brand = i.brand
-                    FROM items AS i
+                        category_id = i.max_category_id,
+                        category_code = i.max_category_code,
+                        brand = i.max_brand
+                    FROM (
+                        SELECT
+                            product_id,
+                            MAX(category_id) AS max_category_id,
+                            MAX(category_code) AS max_category_code,
+                            MAX(brand) AS max_brand
+                        FROM items
+                        GROUP BY product_id
+                    ) AS i
                     WHERE c.product_id = i.product_id;
                     ''')
     cursor.close()
+    conn.commit()
     conn.close()
