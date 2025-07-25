3c8498ff241817e3f5546863c78cf2ceb221d187
Juan carlos IGLESIAS GONZALEZ <jiglesia@f2r8s7.clusters.42paris.fr>
2025-07-25 17:13:00 +0200

    add streamlit

--- Day02/ex00/pie.py
+++ Day02/ex00/pie.py
@@ -1,18 +1,20 @@
 import psycopg2
-import matplotlib.pyplot as plt
+import plotly.express as px
+import streamlit as st
 
-if __name__ == "__main__":
+@st.cache_data
+def get_data():
     conn = psycopg2.connect(database="piscineds", user='jiglesia', password='mysecretpassword', host='127.0.0.1')
     conn.autocommit = False
     cursor = conn.cursor()
 
-    cursor.execute("SELECT event_type FROM customers")
+    cursor.execute("SELECT event_type, COUNT(*) FROM customers GROUP BY event_type")
     data = cursor.fetchall()
     cursor.close()
     conn.close()
-    
-    events = [row[0] for row in data]
-    count  = {event: events.count(event) for event in set(events)}
-    plt.pie(count.values(), labels=count.keys(), autopct='%1.1f%%')
-    plt.show()
+    return data
 
+data = get_data()
+event_types, counts = zip(*data)
+fig = px.pie(names=event_types, values=counts, title='Event Type Distribution')
+st.plotly_chart(fig)
