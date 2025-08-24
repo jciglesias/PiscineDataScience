33205eb9d0b388b03871e7d86f3a4fc647a43303
Juan Carlos Iglesias <jiglesia@student.42.fr>
2025-08-24 18:55:25 +0200

    find inertia and show optimal clusters

--- Day02/ex04/elbow.py
+++ Day02/ex04/elbow.py
@@ -3,6 +3,7 @@ import psycopg2
 import plotly.express as px
 import pandas as pd
 from sklearn.datasets import make_blobs
+from sklearn.cluster import KMeans
 
 query = """
     SELECT 
@@ -28,4 +29,22 @@ def get_data():
 dataset = get_data()
 x,y = make_blobs(n_samples=100, centers=3, cluster_std=0.60, random_state=0)
 kmean_fig = px.scatter(x=x[:, 0], y=x[:, 1], title="K-means Clustering Example")
-st.plotly_chart(kmean_fig, use_container_width=True)
+l_col, r_col = st.columns(2)
+l_col.plotly_chart(kmean_fig, use_container_width=True)
+
+inertia = []
+K = range(1, 11)
+
+for k in K:
+	kmeans = KMeans(n_clusters=k)
+	kmeans.fit(dataset[['total_spent', 'purchase_frequency']])
+	inertia.append(kmeans.inertia_)
+
+inertia_fig = px.line(x=K, y=inertia, labels={'x': 'Number of Clusters (K)', 'y': 'Inertia'}, title='Elbow Method for Optimal K')
+r_col.plotly_chart(inertia_fig, use_container_width=True)
+
+optimal_k = l_col.selectbox("Select the optimal number of clusters (K):", options=K, index=None)
+if optimal_k:
+	a, b = make_blobs(n_samples=100, centers=optimal_k, cluster_std=0.60, random_state=0)
+	fig = px.scatter(x=a[:, 0], y=a[:, 1], title="Optimal K Visualization Example")
+	r_col.plotly_chart(fig, use_container_width=True)
